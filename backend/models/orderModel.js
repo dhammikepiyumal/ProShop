87461cb70cd8f007e2a5b95e8411afca995e11c3
Dhammike Piyumal <dhammikepiyumal@gmail.com>
2021-05-13 19:10:10 +0530

    Data Seeder script

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -28,7 +28,7 @@ const orderSchema = mongoose.Schema(
         product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
-          ref: Product,
+          ref: 'Product',
         },
       },
     ],
