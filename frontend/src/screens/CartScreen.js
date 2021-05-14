074c01081bdd6fcc48144a6425cdb72448f13542
Dhammike Piyumal <dhammikepiyumal@gmail.com>
2021-05-14 21:34:24 +0530

    Add to Cart Functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -15,6 +15,8 @@ const CartScreen = ({ match, location, history }) => {
   const cart = useSelector((state) => state.cart)
   const { cartItems } = cart
 
+  console.log(cartItems)
+
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty))
