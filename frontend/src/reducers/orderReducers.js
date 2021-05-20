7e5503712edb89a386cfa2fc08f29610aa109da9
Dhammike Piyumal <dhammikepiyumal@gmail.com>
2021-05-20 23:52:26 +0530

    Show Orders on Profile

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -9,6 +9,9 @@ import {
   ORDER_PAY_FAIL,
   ORDER_PAY_SUCCESS,
   ORDER_PAY_REQUEST,
+  ORDER_MY_LIST_REQUEST,
+  ORDER_MY_LIST_SUCCESS,
+  ORDER_MY_LIST_FAIL,
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -76,6 +79,27 @@ export const orderPayReducer = (state = {}, action) => {
       }
     case ORDER_PAY_RESET:
       return {}
+    default:
+      return state
+  }
+}
+
+export const orderMyListReducer = (state = { orders: [] }, action) => {
+  switch (action.type) {
+    case ORDER_MY_LIST_REQUEST:
+      return {
+        loading: true,
+      }
+    case ORDER_MY_LIST_SUCCESS:
+      return {
+        loading: false,
+        orders: action.payload,
+      }
+    case ORDER_MY_LIST_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      }
     default:
       return state
   }
